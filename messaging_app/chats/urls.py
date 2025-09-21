6d0930532f709f6f587ad02b069144fc25a24965
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-09-21 14:04:49 +0100

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,1 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from conversations.views import ConversationViewSet, MessageViewSet  # import your viewsets
+
+# Create a router and register your viewsets
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Include API routes with prefix 'api/'
+    path('api/', include(router.urls)),
+]
 
