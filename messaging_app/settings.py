3bba96fa5e346258ba660368c4e2c021474846cc
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-09-26 16:35:45 +0100

    Update settings.py

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -1,5 +1,14 @@
 # messaging_app/settings.py
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',  # Required string
+    'PAGE_SIZE': 20,  # Required string and value
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
+
+
+# messaging_app/settings.py
+
 # ... existing imports and settings ...
 
 INSTALLED_APPS += [
