93b2857b6a0df69b4e117c0b6957b917ee466077
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-09-27 11:29:25 +0100

    Update middleware.py

--- Django-Middleware-0x03/chats/Django-Middleware-0x03/Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/Django-Middleware-0x03/Django-Middleware-0x03/chats/middleware.py
@@ -1,1 +1,18 @@
+# chats/middleware.py
+
+import logging
+from datetime import datetime
+
+# Create or get logger
+logger = logging.getLogger('request_logger')
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user if hasattr(request, 'user') and request.user.is_authenticated else "Anonymous"
+        logger.info(f"{datetime.now()} - User: {user} - Path: {request.path}")
+        response = self.get_response(request)
+        return response
 
