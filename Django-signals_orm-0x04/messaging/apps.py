a1ca5092e8230b388c02a72031623331981483b3
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-09-29 17:50:08 +0100

    Update apps.py

--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -1,1 +1,9 @@
+from django.apps import AppConfig
+
+class MessagingAppConfig(AppConfig):  # Replace 'MessagingApp' with your app name
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'messaging'  # Replace with your app name
+
+    def ready(self):
+        import messaging.signals  # Make sure the signals are imported
 
